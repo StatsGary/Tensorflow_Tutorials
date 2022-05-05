70d8bbc2d978c3e987d60b3963ecfc36b9bbda35
Gary Hutson <g.hutson@nhs.net>
2022-05-05 16:30:42 +0100

    Added massive changes to recurse through list

--- 01_CV_Classification/01_Download_Images.py
+++ 01_CV_Classification/01_Download_Images.py
@@ -6,39 +6,46 @@ import io
 from datetime import datetime as dt
 from PIL import Image
 import time
+import os
 
 # Download the driver from chromedriver website for relevant OS i.e. MAC, Windows, Debian, etc.
 PATH = 'C:/Users/garyh/Documents/GitHub/Tensorflow_Tutorials/01_CV_Classification/chromedriver/chromedriver.exe'
-wd = webdriver.Chrome(PATH)
+wd = webdriver.Chrome(executable_path=PATH)
 
 
-def get_google_images(wd, delay, max_images, url):
-    def scroll_down(wd):
-        wd.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        time.sleep(delay)
+def get_images_from_google(wd, delay, max_images, url):
+	def scroll_down(wd):
+		wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		time.sleep(delay)
 
-    url = url
-    wd.get(url)
+	url = url
+	wd.get(url)
 
-    image_urls = set()
-    while len(image_urls) < max_images:
-        scroll_down(wd)
-        thumbnails = wd.find_elements(By.CLASS_NAME, 'Q4LuWd')
-        # Loop through urls until at max images
-        for img in thumbnails[len(image_urls): max_images]:
-            try:
-                img.click()
-                time.sleep(delay)
-            except:
-                continue
+	image_urls = set()
+	skips = 0
+	while len(image_urls) + skips < max_images:
+		scroll_down(wd)
+		thumbnails = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
 
-            images = wd.find_elements(By.CLASS_NAME, 'n3VNCb')
+		for img in thumbnails[len(image_urls) + skips:max_images]:
+			try:
+				img.click()
+				time.sleep(delay)
+			except:
+				continue
 
-            # Get the larger image, as we are dealing with URLS at the moment
+			images = wd.find_elements(By.CLASS_NAME, "n3VNCb")
+			for image in images:
+				if image.get_attribute('src') in image_urls:
+					max_images += 1
+					skips += 1
+					break
 
+				if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+					image_urls.add(image.get_attribute('src'))
+					##print(f"Found {len(image_urls)}")
 
-
-
+	return image_urls
 
 
 def download_image(down_path, url, file_name, image_type='JPEG',
@@ -62,5 +69,36 @@ def download_image(down_path, url, file_name, image_type='JPEG',
     except Exception as e:
         print(f'Unable to download image from Google Photos due to\n: {str(e)}')
 
-url = 'https://www.google.com/search?q=lewis+grabban&rlz=1C1CHBF_en-GBGB924GB924&sxsrf=ALiCzsZYqtC0Dr5X-fqi3qg8rqxDuI87HQ:1651752199175&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiku9q4qMj3AhUdQ0EAHa5gBkgQ_AUoAnoECAEQBA&biw=2048&bih=1004&dpr=0.94#imgrc=rRrRxw8wQ7YxWM'
-get_google_images(wd, 2, 10, url)
+
+
+if __name__ == '__main__':
+    # Google search URLS
+    google_urls = ['https://www.google.com/search?q=lewis+grabban&rlz=1C1CHBF_en-GBGB924GB924&sxsrf=ALiCzsZYqtC0Dr5X-fqi3qg8rqxDuI87HQ:1651752199175&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiku9q4qMj3AhUdQ0EAHa5gBkgQ_AUoAnoECAEQBA&biw=2048&bih=1004&dpr=0.94#imgrc=rRrRxw8wQ7YxWM',
+                  'https://www.google.com/search?q=philip+zinckernagel&tbm=isch&ved=2ahUKEwiWwcmwzcj3AhUN0oUKHYmmCwUQ2-cCegQIABAA&oq=philipzin&gs_lcp=CgNpbWcQARgAMgYIABAKEBg6BwgjEO8DECc6BQgAEIAEOgYIABAFEB46BAgAEB5QsRBYiRRgpSNoAHAAeACAATqIAdwBkgEBNJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=wuNzYpbYDY2klwSJza4o&bih=1004&biw=2048&rlz=1C1CHBF_en-GBGB924GB924',
+                  'https://www.google.com/search?rlz=1C1CHBF_en-GBGB924GB924&sxsrf=ALiCzsbdBXzzbO-rUCgRzokBwQ3ieCg4kQ:1651763782663&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj-zJLM08j3AhWRi1wKHWriAEUQ_AUoAXoECAIQAw&q=Brennan%20Johnson&lei=SepzYuD4HdTC8gK28IPYCg&biw=1920&bih=969&dpr=1']
+    
+    # Labels for the players
+    labels = ['lewis_grabban', 'philip_zinckernagel', 'brennan_johnson']
+    player_path = 'images/train/'
+    # Make the directory if it doesn't exist
+    for lbl in labels:
+        # Start afresh
+
+        if os.path.exists(player_path + lbl):
+            print(f'Removing detritus directory: {str(lbl)}')
+            os.rmdir(player_path+lbl)
+
+        if not os.path.exists(player_path + lbl):
+            print(f'Making directory: {str(lbl)}')
+            os.makedirs(player_path+lbl)
+
+    for url_current, lbl in zip(google_urls, labels):
+        urls = get_images_from_google(wd, 0, 100, url_current)
+        # Once we have added our urls to empty set then 
+        for i, url in enumerate(urls):
+            download_image(down_path=f'images/train/{lbl}/', 
+                        url=url, 
+                        file_name=str(i+1)+ '.jpg',
+                        verbose=True) 
+    wd.quit()
+
