1fddde6e66a064851bfb22495330545d65d43790
Gary Hutson <g.hutson@nhs.net>
2022-05-06 15:12:44 +0100

    Added split images function

--- 01_CV_Classification/02_Split_Images.py
+++ 01_CV_Classification/02_Split_Images.py
@@ -1,18 +1,26 @@
 import time
 import io
 import splitfolders
+import typing
 
 
 def split_folders(target_folder, ratio, 
                   group_prefix=None, move=False, output_folder='images', seed=123):
+
+    train_ratio, val_ratio, test_ratio = ratio[0], ratio[1], ratio[2]
+    print(f'[SPLIT INITIALISING] Initialising folder split on folder: {target_folder} with train ratio: {train_ratio} validation ratio: {val_ratio} and testing ratio: {test_ratio}.')
     target_folder = target_folder #'images/nottingham_forest'
     output_folder = output_folder #'images'
     splitfolders.ratio(target_folder, output_folder, 
                     seed=seed, ratio=(ratio), 
                     group_prefix=group_prefix, move=move)
+    print(f'[SPLIT COMPLETE] Split of images complete and can be found in folder: {output_folder}')
 
 
-split_folders('images/nottingham_forest', (0.85, 0.10, 0.05))
+
+if __name__ =='__main__':
+    split_folders(target_folder='images/nottingham_forest',
+                  ratio=(0.85, 0.10, 0.05))
 
 
 
